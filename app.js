b7efb5b6d026f0cb8f3b33b0839a3e60db762479
Dan <dpedersen84@gmail.com>
2018-11-12 20:14:38 -0600

    logging inputs from DOM from the ui

--- app.js
+++ app.js
@@ -13,7 +13,26 @@ const budgetController = (() => {
 // UI CONTROLLER
 const uiController = (() => {
 
+    let domStrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
 
+    return {
+        getInput: () => {
+            return {
+                type: document.querySelector(domStrings.inputType).value, // either inc or exp
+                description: document.querySelector(domStrings.inputDescription).value,
+                value: document.querySelector(domStrings.inputValue).value,
+            };
+        },
+
+        getDOMStrings: () => {
+            return domStrings;
+        }
+    }
 
 
 
@@ -25,10 +44,13 @@ const uiController = (() => {
 // GLOBAL APP CONTROLLER
 const controller = ((budgetCtrl, uiCtrl) => {
 
+    let DOM = uiCtrl.getDOMStrings();
+
     let ctrlAddItem = () => {
 
         // 1. Get the field input data
-
+            let input = uiCtrl.getInput();
+            console.log(input)
         // 2. Add the item to budget controller
 
         // 3. Add the new item to UI
@@ -37,10 +59,9 @@ const controller = ((budgetCtrl, uiCtrl) => {
 
         // 5. Display budget
 
-        console.log('adding item!')
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem)
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem)
 
     document.addEventListener('keypress', (event) => {
         if (event.keyCode === 13 || event.which === 13) {
